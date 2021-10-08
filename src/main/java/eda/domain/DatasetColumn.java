411c12a4f64e5ad03d04f6f3b082032a2970ebc1
Jaewoong Kim <rorhcdream@gmail.com>
2021-10-08 16:17:41 +0900

    Rename DatasetColumn member that is a keyword

--- src/main/java/eda/domain/DatasetColumn.java
+++ src/main/java/eda/domain/DatasetColumn.java
@@ -1,9 +1,11 @@
 package eda.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
 @Embeddable
 public class DatasetColumn {
+    @Column(name = "_index")
     private int index;
     private String name;
     private String dataType;
