d012733bf9ff77867f9e93f64f188de9724a5826
Jaewoong Kim <rorhcdream@gmail.com>
2021-10-13 10:17:58 +0900

    Config CORS, JPA Auditing

---
+++ src/main/java/eda/config/WebConfig.java
@@ -0,0 +1,18 @@
+package eda.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Value("${cors-hostname}")
+    String hostName;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http:/3.19.199.190");
+    }
+}
