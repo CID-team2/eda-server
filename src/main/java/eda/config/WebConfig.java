12a8ad887ca354c88158f6439827086a2eee6d8c
Jaewoong Kim <rorhcdream@gmail.com>
2021-10-25 23:03:26 +0900

    Add logging cors hostname

--- src/main/java/eda/config/WebConfig.java
+++ src/main/java/eda/config/WebConfig.java
@@ -1,10 +1,12 @@
 package eda.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Slf4j
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Value("${cors-hostname}")
@@ -12,6 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        log.info("CORS hostname: " + hostName);
         registry.addMapping("/**")
                 .allowedOrigins(hostName);
     }
