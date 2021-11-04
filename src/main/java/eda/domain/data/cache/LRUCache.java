8280701899b590bb677374c676ac9edf35379a73
Jaewoong Kim <rorhcdream@gmail.com>
2021-11-04 18:08:54 +0900

    Make LRUCache thread-safe

--- src/main/java/eda/domain/data/cache/LRUCache.java
+++ src/main/java/eda/domain/data/cache/LRUCache.java
@@ -2,12 +2,14 @@ package eda.domain.data.cache;
 
 import java.util.HashMap;
 import java.util.Optional;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class LRUCache <K, V> implements Cache<K, V> {
     private int size;
     private int maxSize;
     private DoublyLinkedList<CacheElement<K, V>> list = new DoublyLinkedList<>();
     private HashMap<K, DoublyLinkedList.Node<CacheElement<K, V>>> nodeHashMap = new HashMap<>();
+    private ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
 
     public LRUCache(int size) {
         maxSize = size;
@@ -15,28 +17,41 @@ public class LRUCache <K, V> implements Cache<K, V> {
 
     @Override
     public Optional<V> get(K key) {
-        DoublyLinkedList.Node<CacheElement<K, V>> node = nodeHashMap.get(key);
-        if (node == null)
-            return Optional.empty();
+        lock.readLock().lock();
+        try {
+            DoublyLinkedList.Node<CacheElement<K, V>> node = nodeHashMap.get(key);
+            if (node == null)
+                return Optional.empty();
 
-        list.moveToFront(node);
-        return Optional.of(node.value.value);
+            synchronized (this) {
+                list.moveToFront(node);
+            }
+
+            return Optional.of(node.value.value);
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
     @Override
     public void put(K key, V value, int size) {
-        if (size > maxSize)
-            throw new IllegalArgumentException("cache entry size is bigger than cache size");
+        lock.writeLock().lock();
+        try {
+            if (size > maxSize)
+                throw new IllegalArgumentException("cache entry size is bigger than cache size");
 
-        while (this.size + size > maxSize) {
-            CacheElement<K, V> e = list.popBack();
-            nodeHashMap.remove(e.key);
-            this.size -= e.size;
+            while (this.size + size > maxSize) {
+                CacheElement<K, V> e = list.popBack();
+                nodeHashMap.remove(e.key);
+                this.size -= e.size;
+            }
+
+            DoublyLinkedList.Node<CacheElement<K, V>> node = list.pushFront(new CacheElement<>(key, value, size));
+            nodeHashMap.put(key, node);
+            this.size += size;
+        } finally {
+            lock.writeLock().unlock();
         }
-
-        DoublyLinkedList.Node<CacheElement<K, V>> node = list.pushFront(new CacheElement<>(key, value, size));
-        nodeHashMap.put(key, node);
-        this.size += size;
     }
 
     @Override
