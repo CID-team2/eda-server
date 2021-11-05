d25a9be80cfcd8f4efa7295aa76d88971577d8ce
Jaewoong Kim <rorhcdream@gmail.com>
2021-11-05 22:52:23 +0900

    Fix ID types of repositories

--- src/main/java/eda/domain/DatasetRepository.java
+++ src/main/java/eda/domain/DatasetRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface DatasetRepository extends JpaRepository<Dataset, Integer> {
+public interface DatasetRepository extends JpaRepository<Dataset, Long> {
     Optional<Dataset> findByName(String name);
 }
--- src/main/java/eda/domain/FeatureViewRepository.java
+++ src/main/java/eda/domain/FeatureViewRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface FeatureViewRepository extends JpaRepository<FeatureView, Integer> {
+public interface FeatureViewRepository extends JpaRepository<FeatureView, Long> {
     Optional<FeatureView> findByName(String name);
 }
