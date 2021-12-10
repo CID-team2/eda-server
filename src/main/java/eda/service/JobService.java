28459171c56f6d698b1288dbda02e110bb7029b1
Jaewoong Kim <rorhcdream@gmail.com>
2021-12-10 16:52:00 +0900

    Move logics to JobManager and implement checking job list

--- src/main/java/eda/service/JobService.java
+++ src/main/java/eda/service/JobService.java
@@ -1,66 +1,35 @@
 package eda.service;
 
 import eda.domain.DatasetColumn;
-import eda.domain.StatisticEntity;
-import eda.domain.StatisticEntityRepository;
-import eda.domain.worker.Job;
-import eda.domain.worker.JobRepository;
+import eda.domain.worker.JobManager;
 import eda.dto.JobDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class JobService {
-    private final JobRepository jobRepository;
-    private final StatisticEntityRepository statisticEntityRepository;
+    private final JobManager jobManager;
 
     public void addJobs(List<DatasetColumn> jobs) {
-        jobRepository.saveAll(
-                jobs.stream().map(Job::new).toList()
-        );
+        jobManager.addJobs(jobs);
     }
 
     public List<JobDto> getWaitingJobs() {
-        return jobRepository.findAllByStatus(Job.Status.WAITING).stream()
+        return jobManager.getWaitingJobs().stream()
                 .map(JobDto::of)
                 .toList();
     }
 
     @Transactional
     public boolean claimJob(long jobId) {
-        Optional<Job> jobColumnOptional = jobRepository.findById(jobId);
-        if (jobColumnOptional.isEmpty())
-            return false;
-        Job job = jobColumnOptional.get();
-        if (job.getStatus() == Job.Status.PROCESSING)
-            return false;
-        else {
-            job.setStatus(Job.Status.PROCESSING);
-            jobRepository.save(job);
-            return true;
-        }
+        return jobManager.claimJob(jobId);
     }
 
     public boolean finishJob(long jobId) {
-        Optional<Job> jobColumnOptional = jobRepository.findById(jobId);
-        if (jobColumnOptional.isEmpty())
-            return false;
-        Job job = jobColumnOptional.get();
-        if (job.getStatus() == Job.Status.WAITING)
-            return false;
-
-        // refuse when statistic is not calculated
-        for (StatisticEntity se : statisticEntityRepository.findAllByColumn()) {
-            if (se.getModifiedAt().isBefore(job.getModifiedAt()))
-                return false;
-        }
-
-        jobRepository.delete(job);
-        return true;
+        return jobManager.finishJob(jobId);
     }
 }
